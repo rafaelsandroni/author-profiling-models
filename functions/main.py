9bfcc6cc0d7d0e06dfaf319d78f44a9cf2fd443a
rafaelsandroni <rafaelsandronidias@gmail.com>
2018-10-24 02:06:40 +0000

    Reports e baseline2

--- functions/main.py
+++ functions/main.py
@@ -1,9 +1,12 @@
 from functions.datasets import getDatasets
-from functions.grid_baseline1 import reglog
-from functions.grid_baseline1 import mlp
+#from functions.grid_baseline1 import reglog
+from functions.grid_baseline2 import Model
+from functions.plot import ROC
+from functions.metrics import evaluator, reportPath
 from sklearn.preprocessing import label_binarize
+from sklearn import preprocessing
+import datetime
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -36,33 +39,39 @@ def run(task = None, dataset = None):
         
         df_training = pd.read_csv(training_path)#, usecols=cols)
         df_test = pd.read_csv(test_path)#, usecols=cols)
-
-        print('training set: ', df_training.shape)        
-        print(df_training.groupby([label]).size())
-        print()
-        print('test set: ', df_test.shape)        
-        print(df_test.groupby([label]).size())
-        print()
-        # df_training.groupby([label]).size().plot(kind='bar',title='Corpus '+name+' training dataset classes distributions')
-        
+    
         X_train = df_training['text'].values
         y_train, _ = labelEncoder(df_training[label].values)
 
         X_test = df_test['text'].values
         y_test, n_classes = labelEncoder(df_test[label].values)    
         
+        report_path = reportPath(task, name)
+        f = open(report_path, 'a')
+        print("Datetime {0}".format(datetime.datetime.now()), file=f)
+        print("Dataset: {0} and task: {1}".format(name, label), file=f)
+        print(file=f)
+        print('training set: ', df_training.shape, file=f)        
+        print(df_training.groupby([label]).size(), file=f)
+        print(file=f)
+        print('test set: ', df_test.shape, file=f)        
+        print(df_test.groupby([label]).size(), file=f)
+        print(file=f)
+        print("n_classes: {0}".format(n_classes), file=f)        
+        
         del(df_test)
         del(df_training)
         
-        print("n_classes: {0}".format(n_classes)
-              
-        (acc, f1, cm) = grid(X_train, X_test, y_train, y_test, n_classes)
-        print()
+        # (acc, f1, cm) = mlp(X_train, X_test, y_train, y_test, n_classes)
+        clf = Model(task, name, f)
         
+        clf.mlp(X_train, y_train, X_test, y_test, n_classes)
+        
+        f.close()
+        
+        #del(Model)
         del(X_train)
         del(X_test)
         del(y_train)
         del(y_test)
-        
-        print('\n+', '-'*60,'+\n')            
         pass
