97c72e71ac0b218a47e051330f2779bd8e7f985b
rafaelsandroni <rafaelsandronidias@gmail.com>
2018-11-10 01:32:32 +0000

    Adding load function

--- functions/datasets.py
+++ functions/datasets.py
@@ -75,6 +75,32 @@ def loadDataframe(task, dataset, root = None):
 
         # X_test = df_test['text'].values
         # y_test, n_classes = labelEncoder(df_test[label].values)    
-        df = pd.concat([df_training, df_test])
+        #df = pd.concat([df_training, df_test])
+        # return (df_training, df_test)
         
-    return df
+    return (df_training, df_test)
+
+
+def load(task, dataset_name, root = None):
+
+  datasets = getDatasets(task,'df', dataset_name, root = None)
+
+  for i in datasets.iterrows():
+      name = i[1]['dataset_name']
+      label = task
+      ds_path = i[1]['path']
+
+      # load training and test dataframes
+      training_path = ds_path + '/' + i[1]['training']
+      test_path = ds_path + '/' +  i[1]['test']
+
+      df_training = pd.read_csv(training_path)#, usecols=cols)
+      df_test = pd.read_csv(test_path)#, usecols=cols)
+
+      X_train = df_training['text'].values
+      y_train = df_training[task].values
+
+      X_test = df_test['text'].values
+      y_test = df_test[task].values
+
+  return (X_train, X_test, y_train, y_test)
