011d377d35be5d3a832a6c2cd71ef21c1496e83a
Rafael Sandroni <rafaelsandronidias@gmail.com>
2018-11-19 15:57:52 -0200

    Fix cnn arch

--- functions/cnn_model.py
+++ functions/cnn_model.py
@@ -117,14 +117,14 @@ def build_simple_cnn(num_words, max_seq_length, filter_sizes=[3,4,5], feature_ma
         feature_map = feature_maps[ix]
         filter_size = filter_sizes[ix]
 
-        #if ix == 0:
-            #model.add(
-                #Conv1D(emb_dim, kernel_size=filter_size, activation='relu', strides=1, padding='same', kernel_regularizer=regularizers.l2(0.03),input_shape=(emb_dim, max_seq_length)
-            #)
-        #else:
-        model.add(        
-            Conv1D(emb_dim, kernel_size=filter_size, activation='relu', strides=1, padding='same', kernel_regularizer=regularizers.l2(0.03))
-        )
+        if ix == 0:
+            model.add(
+                Conv1D(emb_dim, kernel_size=filter_size, activation='relu', strides=1, padding='same', kernel_regularizer=regularizers.l2(0.03),input_shape=(emb_dim, max_seq_length)
+            )
+        else:
+            model.add(        
+                Conv1D(emb_dim, kernel_size=filter_size, activation='relu', strides=1, padding='same', kernel_regularizer=regularizers.l2(0.03))
+            )
 
         model.add(
             #MaxPooling1D(pool_size=2, strides=1, padding='valid')
