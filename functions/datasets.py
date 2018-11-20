22468551002213dd33e3ed7d4a55653527f79a5c
Rafael Sandroni <rafaelsandronidias@gmail.com>
2018-11-20 09:24:58 -0200

    Fixing dataset function

--- functions/datasets.py
+++ functions/datasets.py
@@ -84,29 +84,28 @@ def loadDataframe(task, dataset, root = None):
 
 def load(task, dataset_name, root = None):
 
-    datasets = getDatasets(task,'df', dataset_name, root = None)
+    datasets = getDatasets(task,'df', dataset_name, root)
     
     X_train, X_test, y_train, y_test = [], [], [], []
-    
-    for i in datasets.iterrows():    
 
-        name = i[1]['dataset_name']
-        label = task
-        ds_path = i[1]['path']
+    arr = datasets.values
+    name = arr[0][0]
+    ds_path = arr[0][1]
+    label = arr[0][2]
+    training_file = arr[0][3]
+    test_file = arr[0][4]
 
-        # load training and test dataframes
-        training_path = ds_path + '/' + i[1]['training']
-        test_path = ds_path + '/' +  i[1]['test']
+    # load training and test dataframes
+    training_path = ds_path + '/' + training_file
+    test_path = ds_path + '/' +  test_file
 
-        df_training = pd.read_csv(training_path)#, usecols=cols)
-        df_test = pd.read_csv(test_path)#, usecols=cols)
+    df_training = pd.read_csv(training_path)#, usecols=cols)
+    df_test = pd.read_csv(test_path)#, usecols=cols)
 
-        X_train = df_training['text'].values
-        y_train = df_training[label].values
+    X_train = df_training['text'].values
+    y_train = df_training[label].values
 
-        X_test = df_test['text'].values
-        y_test = df_test[label].values
-
-        print(X_train.shape)
+    X_test = df_test['text'].values
+    y_test = df_test[label].values
 
     return (X_train, X_test, y_train, y_test)
